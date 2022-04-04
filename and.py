6bd31d3ed70476d6d6a8306cb13038a1ecf7d129
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2022-04-05 01:57:22 +0600

    add and or are made as functionwise and organised

--- and.py
+++ and.py
@@ -3,27 +3,27 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, modelfilename,plotfilename):
+    df = pd.DataFrame(data)
+    print(df)
+    X,y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+    _ = model.total_loss()
 
-df_AND = pd.DataFrame(AND)
+    save_model(model, filename=modelfilename)
+    save_plot(df, file_name=plotfilename, model=model)
 
-print(df_AND)
-
-
-X,y = prepare_data(df_AND)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model_AND = Perceptron(eta=ETA, epochs=EPOCHS)
-model_AND.fit(X, y)
-
-_ = model_AND.total_loss()
-
-save_model(model_AND, filename="and.model")
-save_plot(df_AND, file_name="and.png", model=model_AND)
+if __name__=="__main__": # << entry point <<
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    
+    main(data=AND, eta=ETA, epochs=EPOCHS, modelfilename="and.model", plotfilename="and.png")
+    
