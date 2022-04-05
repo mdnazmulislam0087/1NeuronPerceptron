f41b2fc40b16f61233d04033965f98e7ce8d58fb
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2022-04-05 14:26:05 +0600

    logger added in xor, or, nand

--- and.py
+++ and.py
@@ -7,10 +7,19 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
+import logging
+import os 
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+#logging.basicConfig(level=logging.INFO, format=logging_str)
+
 
 def main(data, eta, epochs, modelfilename,plotfilename):
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(f"The dataframe is : {df}")
     X,y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
@@ -30,5 +39,13 @@ if __name__=="__main__": # << entry point <<
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
     
-    main(data=AND, eta=ETA, epochs=EPOCHS, modelfilename="and.model", plotfilename="and.png")
+    #main(data=AND, eta=ETA, epochs=EPOCHS, modelfilename="and.model", plotfilename="and.png")
+    try:
+        
+        logging.info(">>>>> starting training >>>>>")
+        main(data=AND, eta=ETA, epochs=EPOCHS, modelfilename="and.model", plotfilename="and.png")
+        logging.info("<<<<< training done successfully<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e 
     
--- xor.py
+++ xor.py
@@ -8,10 +8,18 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
+import logging
+import os 
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+#logging.basicConfig(level=logging.INFO, format=logging_str)
 
 def main(data, eta, epochs, modelfilename,plotfilename):
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(df)
     X,y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
@@ -31,6 +39,12 @@ if __name__=="__main__": # << entry point <<
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
     
-    main(data=XOR, eta=ETA, epochs=EPOCHS, modelfilename="xor.model", plotfilename="xor.png")
-    
+    #main(data=XOR, eta=ETA, epochs=EPOCHS, modelfilename="xor.model", plotfilename="xor.png")
+    try:
+        logging.info(">>>>> starting training >>>>>")
+        main(data=XOR, eta=ETA, epochs=EPOCHS, modelfilename="xor.model", plotfilename="xor.png")
+        logging.info("<<<<< training done successfully<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e    
     
